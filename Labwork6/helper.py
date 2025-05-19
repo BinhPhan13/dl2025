0c421af83f87e42a4c56376d3de5656a0cee44ea
Binh Phan <ptbinh2003@gmail.com>
2025-05-19 19:52:08 +0700

    feat: flatten layer

--- Labwork6/helper.py
+++ Labwork6/helper.py
@@ -1,5 +1,6 @@
 from math import exp
 from arr import Array
+from mat import Matrix
 
 def sigmoid(x: float):
     return 1 / (1 + exp(-x))
@@ -15,4 +16,22 @@ def mce_der(ytrue: int, scores: Array[float]):
     pred = Array(exp(s) for s in scores)[ytrue]
     grad[ytrue] = -1/pred * pred * (1-pred)
     return grad
+
+
+class Flatten:
+    def __call__(self, x: Array[Matrix]) -> Array[float]:
+        self.shape = x[0].shape
+        out = Array(d for m in x for d in m)
+        self.n_out = len(out)
+        return out
+
+    def grad(self, grad: Array[float]) -> Array[Matrix]:
+        assert len(grad) == self.n_out
+        k = self.shape[0] * self.shape[1]
+        loop = range(0, self.n_out, k)
+        out = Array(
+            Matrix(grad[i+j] for j in range(k)).on(*self.shape)
+            for i in loop
+        )
+        return out
 
